51762ceadfa9477aebba56bcd064bed4c25e182a
Kapilesh VS <kapilesh1531@gmail.com>
2026-09-10 16:08:08 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const MONGO_URI =
 app.use((req, res, next) => {
 
   res.header(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5173"
-  );
+  "Access-Control-Allow-Origin",
+  process.env.CLIENT_URL || "http://localhost:5173"
+);
 
   res.header(
     "Access-Control-Allow-Methods",
